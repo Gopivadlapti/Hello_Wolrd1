b55d3a5950134c911716da2ac8118716680438f0
Gopivadlapti <vadlapatigopi456@gmail.com>
2016-06-10 12:21:49 +0530

    Adding batch script

---
+++ job.bat
@@ -0,0 +1,5 @@
+cd C:\"Program Files (x86)"\Jenkins\jobs\Ant_Job\workspace
+
+mkdir Artifacts
+
+cp C:\"Program Files (x86)"\Jenkins\jobs\Ant_Job\builds\lastUnsuccessfulBuild\log  C:\"Program Files (x86)"\Jenkins\jobs\Ant_Job\workspace\Artifacts
